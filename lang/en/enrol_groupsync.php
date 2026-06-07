2e727fba74f02563fc08619cfddc36de3bdbe73c
Andrew Chandler <andrew.chandler@skills-group.org>
2026-06-08 11:51:50 +1200

    Add 'synctask' lang string.

--- lang/en/enrol_groupsync.php
+++ lang/en/enrol_groupsync.php
@@ -28,3 +28,4 @@ $string['instanceexists'] = 'Cohort is already synchronised with selected group'
 $string['pluginname'] = 'Cohort members to group';
 $string['pluginname_desc'] = 'Cohort to group sync plugin synchronises cohort members with group, this plugin does not enrol anybody.';
 $string['privacy:metadata:subsystem:group'] = 'The plugin adds the user into the course groups based on their cohort membership.';
+$string['synctask'] = 'Synchronise cohort to group memberships';
